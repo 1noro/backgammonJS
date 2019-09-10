83c469b9954e9414fb8deb6d6f5bd66818c770fe
inoro <ssppaamm@protonmail.com>
2019-09-11 01:46:55 +0200

    a lot of things

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,8 @@
 
 function main() {
     console.log("[MAIN] Start...");
+    var
+        bgc = document.getElementById('backgammonCanvas'),
+        m = 5; //multiplicador
+    draw_board(bgc, m);
 }
