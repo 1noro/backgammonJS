15c32d9dcef6d78a22222bf1274067e9afaa82cf
inoro <ssppaamm@protonmail.com>
2019-09-11 01:52:11 +0200

    borrados comentarios

--- public_html/js/draw_board.js
+++ public_html/js/draw_board.js
@@ -14,95 +14,12 @@ function draw_board(bgc, m) {
     // ctx.fillRect(20*m, 260*m, 260*m, 20*m); // - borde inferior
     // ctx.fillRect(140*m, 20*m, 20*m, 240*m); // - borde medio
 
-    // parte izquierda
-    // ctx.moveTo(20*m, 20*m);
-    // ctx.lineTo(20*m, 260*m);
-    // ctx.lineTo(140*m, 260*m);
-    // ctx.lineTo(140*m, 20*m);
-    // ctx.lineTo(20*m, 20*m);
-    // ctx.beginPath();
-    // ctx.rect(20, 20, 150, 100);
-    // ctx.stroke();
-
-    // parte derecha
-    // ctx.moveTo(160*m, 20*m);
-    // ctx.lineTo(160*m, 260*m);
-    // ctx.lineTo(280*m, 260*m);
-    // ctx.lineTo(280*m, 20*m);
-    // ctx.lineTo(160*m, 20*m);
-    // ctx.stroke();
-
     // PARTES DEL TABLERO
     ctx.fillStyle = "#2c3037";
     ctx.fillRect(20*m, 20*m, 120*m, 240*m); // parte izquierda
     ctx.fillRect(160*m, 20*m, 120*m, 240*m); // parte derecha
 
-    // picos parte izquierda arriba
-    // ctx.moveTo(20*m, 20*m);
-    // ctx.lineTo(30*m, 110*m);
-    // ctx.lineTo(40*m, 20*m);
-    // ctx.lineTo(50*m, 120*m);
-    // ctx.lineTo(60*m, 20*m);
-    // ctx.lineTo(70*m, 110*m);
-    // ctx.lineTo(80*m, 20*m);
-    // ctx.lineTo(90*m, 120*m);
-    // ctx.lineTo(100*m, 20*m);
-    // ctx.lineTo(110*m, 110*m);
-    // ctx.lineTo(120*m, 20*m);
-    // ctx.lineTo(130*m, 120*m);
-    // ctx.lineTo(140*m, 20*m);
-    // ctx.stroke();
-
-    // picos parte izquierda arriba
-    // - pico 1 (blanco)
-    ctx.beginPath();
-    ctx.moveTo(20*m, 20*m);
-    ctx.lineTo(30*m, 110*m);
-    ctx.lineTo(40*m, 20*m);
-    ctx.closePath();
-    ctx.fillStyle = "#aab1be";
-    ctx.fill();
-    // - pico 2 (rojo)
-    ctx.beginPath();
-    ctx.moveTo(40*m, 20*m);
-    ctx.lineTo(50*m, 120*m);
-    ctx.lineTo(60*m, 20*m);
-    ctx.closePath();
-    ctx.fillStyle = "#df6b74";
-    ctx.fill();
-    // - pico 3 (blanco)
-    ctx.beginPath();
-    ctx.moveTo(60*m, 20*m);
-    ctx.lineTo(70*m, 110*m);
-    ctx.lineTo(80*m, 20*m);
-    ctx.closePath();
-    ctx.fillStyle = "#aab1be";
-    ctx.fill();
-    // - pico 4 (rojo)
-    ctx.beginPath();
-    ctx.moveTo(80*m, 20*m);
-    ctx.lineTo(90*m, 120*m);
-    ctx.lineTo(100*m, 20*m);
-    ctx.closePath();
-    ctx.fillStyle = "#df6b74";
-    ctx.fill();
-    // - pico 5 (blanco)
-    ctx.beginPath();
-    ctx.moveTo(100*m, 20*m);
-    ctx.lineTo(110*m, 110*m);
-    ctx.lineTo(120*m, 20*m);
-    ctx.closePath();
-    ctx.fillStyle = "#aab1be";
-    ctx.fill();
-    // - pico 6 (rojo)
-    ctx.beginPath();
-    ctx.moveTo(120*m, 20*m);
-    ctx.lineTo(130*m, 120*m);
-    ctx.lineTo(140*m, 20*m);
-    ctx.closePath();
-    ctx.fillStyle = "#df6b74";
-    ctx.fill();
-
+    // PICOS
     var cuadrante = 1;
     var rojo = false;
     var px = 20;
@@ -111,7 +28,7 @@ function draw_board(bgc, m) {
         while (pico <= 6) {
             ctx.beginPath();
             ctx.moveTo(px*m, 20*m);
-            rojo?ctx.lineTo((px+10)*m, 110*m):ctx.lineTo((px+10)*m, 120*m);
+            rojo?ctx.lineTo((px+10)*m, 120*m):ctx.lineTo((px+10)*m, 110*m);
             ctx.lineTo((px+20)*m, 20*m);
             ctx.closePath();
             rojo?ctx.fillStyle = "#df6b74":ctx.fillStyle = "#aab1be";
@@ -119,7 +36,7 @@ function draw_board(bgc, m) {
 
             ctx.beginPath();
             ctx.moveTo(px*m, 260*m);
-            rojo?ctx.lineTo((px+10)*m, 150*m):ctx.lineTo((px+10)*m, 160*m);
+            rojo?ctx.lineTo((px+10)*m, 160*m):ctx.lineTo((px+10)*m, 150*m);
             ctx.lineTo((px+20)*m, 260*m);
             ctx.closePath();
             rojo?ctx.fillStyle = "#aab1be":ctx.fillStyle = "#df6b74";
